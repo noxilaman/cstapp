db5109e030800c429acccb2570a74a6bc6f19161
Pong CPE <pong.cpe@gmail.com>
2022-05-12 12:48:03 +0700

    home edit

--- resources/views/students/qrcode.blade.php
+++ resources/views/students/qrcode.blade.php
@@ -6,15 +6,16 @@
     <br/>
     <br/>
     <div  class="row" >
-        <div class="col-md-1"></div>
-        <div class="col-md-4" style="border:1px solid black; text-align:center;">
-            <h1>{{ $projectcompany->project->name }} ของบริษัท {{ $projectcompany->company->name }} </h1>
+        <div class="col-md-3"></div>
+        <div class="col-md-6" style="text-align:center;">
+            <h1>{{ $projectcompany->project->name }}  </h1>
+            <h1>สมัครสมาชิกของโรงแรม{{ $projectcompany->company->name }} </h1><br/>
             @php
                 $url = url('students/register/'.$projectcompany->id);
                 echo QrCode::size(250)->generate($url);
             @endphp
         </div>
-        <div class="col-md-7"></div>
+        <div class="col-md-3"></div>
     </div>
     <br/>
     <br/>
--- resources/views/students/thankyou.blade.php
+++ resources/views/students/thankyou.blade.php
@@ -1,7 +1,8 @@
  @extends('layouts.register')
 
 @section('content')
-<h1>ขอบคุณที่ลงทะบียน<br/>{{ $pjcomstd->project->name }} ของบริษัท {{ $pjcomstd->company->name }} </h1>
+<h1>ขอบคุณที่ลงทะบียน {{ $pjcomstd->project->name }}</h1>
+<h1>ของโรงแรม{{ $pjcomstd->company->name }} </h1>
                     <p>กรุณาCapหน้าจอมือถือ หรือ จด User / Pass เพื่อใช้ในการเข้าระบบครั้งต่อไป</p>
                         <div class="col-md-12 col-sm-12">
                     <div class="block">
